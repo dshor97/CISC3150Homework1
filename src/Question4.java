407fef279a45f1af0944d2c9eec6ab0aa4309e7b
Dayton Shor <dayton.shor@gmain.com>
2017-09-01 16:59:58 -0400

    -- replace text file with Question2TextFile

--- src/Question4.java
+++ src/Question4.java
@@ -1,5 +1,9 @@
 public class Question4 {
     public static void main (String[] args){
-
+        if(args.length <= 0){
+            System.out.println("There aren't enough arguments. Need a file name");
+        } else {
+            System.out.println("There is an argument.");
+        }
     }
 }
